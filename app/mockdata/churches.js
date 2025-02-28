b7b6a60e23390618f7adfc8e9985887ce2ece274
BaptistHub <anonlayer0@gmail.com>
2025-02-28 00:01:13 -0800

    Update churches.js

--- app/mockdata/churches.js
+++ app/mockdata/churches.js
@@ -1,12 +1,12 @@
 export const churchlist = {
-        'Faithful Word': {
-            logo : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbhlcuMDW7P3xMCoilrapWEEsXJj_ltJE_5g&s',
+        'Mountain baptist': {
+            logo : 'https://yt3.googleusercontent.com/78qTMLhYFLdoIIIfxk_Q14ZQh9nfCD1lt3crIbRx5QaGv58CZ9YBEKL1KyDkRuA2GLZH9MFWIA=s900-c-k-c0x00ffffff-no-rj',
         },
-    'Stedfast': {
-            logo : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcToTNQbcrNmD2RxReFQtyhRkxLgrNL-hsqX3A&s',
+    'Cornerstone baptist': {
+            logo : 'https://artwork.subsplash.com/images/cdn/NTB972/512/512/ios/icon.png',
         },
-    'Verity': {
-            logo : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtjePfqs4e1CU4x-v1RrIIOfjs5Xq1a90fKQ&s',
+    'Love baptist': {
+            logo : 'https://scontent.fyvr2-1.fna.fbcdn.net/v/t39.30808-6/336865782_924603525393510_5739936762375763443_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=J0CI5009uvsQ7kNvgEuPFYg&_nc_oc=Adh6nVQYB_r92ph-gpgD13_BJZiKZrQxKGAEAbxsrbuQN4_LFTGjQrxsXSpdR8hpEKF_xz08g8-sO5A1nXIorCiF&_nc_zt=23&_nc_ht=scontent.fyvr2-1.fna&_nc_gid=AYrg1KonAKNDwqIlBKfVr0E&oh=00_AYCoEz9ueHDxtuXTCnV9wuFQVWAKwmjdzwQXGxVZJXc5ow&oe=67C73171',
         },
  
         'First works': {
